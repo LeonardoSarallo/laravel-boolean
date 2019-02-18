98a048da14d831b2625d18126b1023593f676c1d
LeonardoSarallo <leo.sarallo@gmail.com>
2019-02-18 16:30:37 +0100

    oknavbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <img src="{{ asset('img/logo.png')}}" alt="">
+        </div>
+        <div class="header-right">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+              <div class="border">
+
+              </div>
+            </li>
+            <li>
+              <a href="#">Corso</a>
+            </li>
+            <li>
+              <a href="#">Metodo</a>
+            </li>
+            <li>
+              <a href="#">Dopo il corso</a>
+            </li>
+            <li>
+              <a href="#">Faq</a>
+            </li>
+            <li>
+              <a class="button"href="#">Iscriviti</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </header>
+    <div class="jumbo">
+      <div class="container">
+        <div class="jumbo-left">
+
+        </div>
+        <div class="jumbo-right">
+          
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
